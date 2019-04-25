142adc5e0396f2bce324e3b40fd0ad5680e8a8ec
Gifford Nowland <hi@giffordnowland.com>
2019-04-25 17:36:07 -0400

    Abstract image loader function

--- scripts/site.js
+++ scripts/site.js
@@ -7,8 +7,19 @@
 
 const core = require('@squarespace/core');
 
-window.addEventListener('DOMContentLoaded', function() {
-
+// SquareSpace Image Loader
+// https://developers.squarespace.com/imageloader
+//
+// "Our image loader will then help render images at the
+// proper size, even on retina displays. The ImageLoader
+// can also be used to fit or fill an image inside parent
+// container, where it automatically determines which image
+// size to use depending on the current dimensions of the
+// container."
+//
+// Add the imageLoader JSON-T formatter to an img tag
+// <img {@| image - meta} />
+const loadAllImages = () => {
   const images = document.querySelectorAll('img[data-src]');
 
   for (let i = 0; i < images.length; i++) {
@@ -16,5 +27,14 @@ window.addEventListener('DOMContentLoaded', function() {
       load: true
     });
   }
+}
 
+// The event subscription that fires when the page is ready
+window.addEventListener('DOMContentLoaded', function () {
+  loadAllImages();
+});
+
+// The event subscription that fires when the page is resized
+window.addEventListener('resize', function () {
+  loadAllImages();
 });
